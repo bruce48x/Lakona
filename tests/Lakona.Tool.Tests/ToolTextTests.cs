11405fff02fbf6a6165a10a300caba1855e2aefe
bruce <bruce48@foxmail.com>
2026-06-08 14:35:13 +0800

    chore: bump Lakona.Tool version to 0.7.5

--- tests/Lakona.Tool.Tests/ToolTextTests.cs
+++ tests/Lakona.Tool.Tests/ToolTextTests.cs
@@ -775,7 +775,7 @@ public sealed class ToolTextTests
             .Single()
             .Value;
 
-        Assert.Equal("0.7.4", version);
+        Assert.Equal("0.7.5", version);
     }
 
     [Fact]
