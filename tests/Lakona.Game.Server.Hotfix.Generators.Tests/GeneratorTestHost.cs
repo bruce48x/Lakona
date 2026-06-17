4de4682f7d337b16ec848ddc2e6531df256b46d5
bruce <bruce48@foxmail.com>
2026-06-17 11:55:11 +0800

    Reapply "Fix hotfix generator test references"
    This reverts commit 3de0983f2fd45cc48652fa1afdaa8f17c2200874.

--- tests/Lakona.Game.Server.Hotfix.Generators.Tests/GeneratorTestHost.cs
+++ tests/Lakona.Game.Server.Hotfix.Generators.Tests/GeneratorTestHost.cs
@@ -67,6 +67,7 @@ internal static class GeneratorTestHost
                 MetadataReference.CreateFromFile(typeof(Lakona.Game.Server.Hotfix.HotfixServiceCall<>).Assembly.Location),
                 MetadataReference.CreateFromFile(typeof(IServiceProvider).Assembly.Location),
                 MetadataReference.CreateFromFile(typeof(Lakona.Rpc.Server.RpcSession).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Microsoft.Extensions.Configuration.IConfiguration).Assembly.Location),
                 MetadataReference.CreateFromFile(typeof(Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions).Assembly.Location)
             })
             .Distinct(MetadataReferencePathComparer.Instance)
