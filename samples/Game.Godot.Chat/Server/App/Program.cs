560e57756f2ce8f98d79885baa526b4e14c9c055
bruce <bruce48@foxmail.com>
2026-06-21 17:28:15 +0800

    refactor: hide chat session lifecycle app wiring

--- samples/Game.Godot.Chat/Server/App/Program.cs
+++ samples/Game.Godot.Chat/Server/App/Program.cs
@@ -2,11 +2,8 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Server.App.Generated;
-using Server.App.Hosting;
-using Server.App.Hotfix;
 using Lakona.Game.Server.Features;
 using Lakona.Game.Server.Hosting;
-using Lakona.Game.Server.Hotfix.Abstractions;
 using Lakona.Game.Server.Sessions;
 
 return await LakonaGameServer.RunAsync(args, server => server
@@ -17,8 +14,6 @@ return await LakonaGameServer.RunAsync(args, server => server
         {
             options.DisconnectedSessionRetention = TimeSpan.FromSeconds(30);
         });
-        services.AddSingleton<ChatHotfixRuntimeEvents>();
-        services.AddSingleton<IHotfixRequiredServiceContracts, ChatRuntimeRequiredServiceContracts>();
-        services.AddSingleton<IGameSessionLifecycleHandler, ChatSessionLifecycleBridge>();
+        services.AddLakonaGameSessionHotfixLifecycle();
     })
     .UseGeneratedHotfixServices());
--- samples/Game.Godot.Chat/Server/Hotfix/Chat/ChatSessionLifecycle.cs
+++ samples/Game.Godot.Chat/Server/Hotfix/Chat/ChatSessionLifecycle.cs
@@ -1,17 +1,16 @@
 using Server.App.Chat;
-using Server.App.Hotfix;
 using Lakona.Game.Server.Actors;
 using Lakona.Game.Server.Hotfix;
 using Lakona.Game.Server.Hotfix.Abstractions;
 
 namespace Server.Hotfix.Chat
 {
-    [HotfixService(typeof(IChatRuntimeService))]
-    internal sealed class ChatRuntimeService
+    [HotfixLifecycle(typeof(IGameSessionLifecycle))]
+    internal sealed class ChatSessionLifecycle
     {
         private static readonly ActorId RoomId = ActorId.From("chat:global");
 
-        public static async ValueTask SessionExpiredAsync(HotfixServiceCall<ChatSessionExpiredRequest> call)
+        public static async ValueTask SessionExpiredAsync(HotfixLifecycleCall<GameSessionExpiredRequest> call)
         {
             var connectionId = call.Request.ConnectionId;
             if (string.IsNullOrWhiteSpace(connectionId))
